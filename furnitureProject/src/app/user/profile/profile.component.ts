4e4f230161aa0503dd410b97ebbc9d420894108c
Alexander Chemishki <alexander.chemishki@gmail.com>
2025-01-22 16:14:10 +0300

    making profile component

--- furnitureProject/src/app/user/profile/profile.component.ts
+++ furnitureProject/src/app/user/profile/profile.component.ts
@@ -1,11 +1,12 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-
+  isEditing = true;
 }
