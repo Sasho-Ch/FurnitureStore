e22aede22bce39c9594f95e8294d2c63352375e6
Alexander Chemishki <alexander.chemishki@gmail.com>
2025-02-10 14:46:39 +0300

    Add error page

--- furnitureProject/src/app/app.routes.ts
+++ furnitureProject/src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { NewsComponent } from './news/news.component';
 import { YourFurnitureComponent } from './furniture/your-furniture/your-furniture.component';
 import { FurnitureDetailsComponent } from './furniture/furniture-details/furniture-details.component';
 import { EditFurnitureComponent } from './furniture/edit-furniture/edit-furniture.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -17,23 +18,26 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent },
   {
-     path: 'gallery',
-     children: [
-      {path: '', component: GalleryComponent},
+    path: 'gallery',
+    children: [
+      { path: '', component: GalleryComponent },
       {
         path: ':furnitureId',
         component: FurnitureDetailsComponent,
-      }
-     ], 
+      },
+    ],
   },
 
   { path: 'add-furniture', component: AddFurnitureComponent },
   { path: 'edit-furniture/:furnitureId', component: EditFurnitureComponent },
-  { path: 'your-furniture', 
+  {
+    path: 'your-furniture',
     children: [
-      {path: '', component: YourFurnitureComponent},
-      {path: ':furnitureId', component: FurnitureDetailsComponent},
-    ]
-     },
+      { path: '', component: YourFurnitureComponent },
+      { path: ':furnitureId', component: FurnitureDetailsComponent },
+    ],
+  },
   { path: 'news', component: NewsComponent },
+  { path: '404', component: ErrorComponent },
+  { path: '**', redirectTo: '/404' },
 ];
