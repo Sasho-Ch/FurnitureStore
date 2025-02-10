ae3e2c0241c74d61a078b3df15bbe41c67c047bc
Alexander Chemishki <alexander.chemishki@gmail.com>
2025-02-10 16:44:47 +0300

    register component error handling

--- furnitureProject/src/app/user/register/register.component.ts
+++ furnitureProject/src/app/user/register/register.component.ts
@@ -7,6 +7,9 @@ import {
 } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service';
+import { emailValidator } from '../../utils/email.validator';
+import { DOMAINS } from '../../constants';
+import { matchPasswordsValidator } from '../../utils/match-passwords.validator';
 
 @Component({
   selector: 'app-register',
@@ -16,25 +19,75 @@ import { UserService } from '../user.service';
 })
 export class RegisterComponent {
   form = new FormGroup({
-    username: new FormControl(''),
-    email: new FormControl(''),
-    tel: new FormControl(''),
-    password: new FormControl(''),
-    rePassword: new FormControl(''),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+    email: new FormControl('', [Validators.required, emailValidator(DOMAINS)]),
+    tel: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    passGroup: new FormGroup(
+      {
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(5),
+        ]),
+        rePassword: new FormControl('', [Validators.required]),
+      },
+      {
+        validators: [matchPasswordsValidator('password', 'rePassword')],
+      }
+    ),
   });
 
   constructor(private userService: UserService, private router: Router) {}
+
+  isFieldTextMissing(controlName: string) {
+    return (
+      this.form.get(controlName)?.touched &&
+      this.form.get(controlName)?.errors?.['required']
+    );
+  }
+
+  get isNotMinLength() {
+    return (
+      this.form.get('username')?.touched &&
+      this.form.get('username')?.errors?.['minlength']
+    );
+  }
+
+  get isTelNotMinLength() {
+    return (
+      this.form.get('tel')?.touched &&
+      this.form.get('tel')?.errors?.['minlength']
+    );
+  }
+
+  get isEmailNotValid() {
+    return (
+      this.form.get('email')?.touched &&
+      this.form.get('email')?.errors?.['emailValidator']
+    );
+  }
+
+  get passGroup() {
+    return this.form.get('passGroup');
+  }
 
   register() {
     if (this.form.invalid) {
       return;
     }
 
-    const {username, email, tel,password, rePassword} = this.form.value;
-    this.userService.register(username!, email!, tel!, password!, rePassword!)
-    .subscribe(() => {
-
-      this.router.navigate(['/'])
-    })
+    const {
+      username,
+      email,
+      tel,
+      passGroup: { password, rePassword } = {},
+    } = this.form.value;
+    this.userService
+      .register(username!, email!, tel!, password!, rePassword!)
+      .subscribe(() => {
+        this.router.navigate(['/']);
+      });
   }
 }
